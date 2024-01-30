2bc73a87e2f7118b00ad95a321df5f9cb93aabd0
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-01-30 18:27:00 -0300

    aiunda falta

--- fe_lab_clinicas_core/lib/src/helpers/message.dart
+++ fe_lab_clinicas_core/lib/src/helpers/message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -24,3 +25,35 @@ final class Messages {
     );
   }
 }
+
+mixin MessageStateMixin {
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _errorMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+}
+
+mixin MessageViewViewMixin<T extends StatefulWidget> on State<T> {}

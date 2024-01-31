4bf0eab87c5dae0f45067a5ff45b4ab4a5e6241c
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-01-31 04:29:25 -0300

    dia1aula7

--- fe_lab_clinicas_core/lib/fe_lab_clinicas_core.dart
+++ fe_lab_clinicas_core/lib/fe_lab_clinicas_core.dart
@@ -4,3 +4,5 @@ export 'src/fp/either.dart';
 export 'src/fp/nil.dart';
 export 'src/fp/unit.dart';
 export 'src/helpers/message.dart';
+export 'src/restClient/rest_client.dart';
+export 'src/constantes/local_storage_constantes.dart';

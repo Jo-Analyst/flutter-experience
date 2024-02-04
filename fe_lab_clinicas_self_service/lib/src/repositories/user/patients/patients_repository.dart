d1ea46743cbfcb1059b9c27ea5f1020229318542
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-02-04 05:53:32 -0300

    FE - Jan D3A3

--- fe_lab_clinicas_self_service/lib/src/repositories/user/patients/patients_repository.dart
+++ fe_lab_clinicas_self_service/lib/src/repositories/user/patients/patients_repository.dart
@@ -3,5 +3,5 @@ import 'package:fe_lab_clinicas_self_service/src/model/patient_model.dart';
 
 abstract interface class PatientsRepository {
   Future<Either<RepositoryException, PatientModel>> findPatientByDocument(
-      String document) {}
+      String document);
 }
